9a0026ac86584e27aca38a8e64a491ebf17ebf02
akita-t <aknor1004@gmail.com>
2025-12-07 10:49:59 +0900

    feat(oil): enable auto refresh on external file changes

--- lua/kickstart/plugins/oil.lua
+++ lua/kickstart/plugins/oil.lua
@@ -2,10 +2,10 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('oil').setup {
+      watch_for_changes = true,
       view_options = {
         show_hidden = true,
       },
